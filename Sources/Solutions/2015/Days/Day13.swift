130ab1f9f7714b5b9f86eeb864b160e349f5b99d
rbruinier <rjbruinier@gmail.com>
2022-02-11 19:58:35 +0100

    Day 13 & 14 2015

--- Sources/Solutions/2015/Days/Day13.swift
+++ Sources/Solutions/2015/Days/Day13.swift
@@ -7,17 +7,97 @@ final class Day13Solver: DaySolver {
     private var input: Input!
 
     private struct Input {
+        let pairs: [String: [Pair]]
+    }
+
+    private struct Pair {
+        let a: String
+        let b: String
+
+        let score: Int
+    }
+
+    private func calculateHappiness(seating: [String], pairs: [String: [Pair]]) -> Int {
+        var score = 0
+
+        for (index, name) in seating.enumerated() {
+            let scoring = pairs[name]!
+
+            let previousIndex = (index >= 1) ? index - 1 : seating.count - 1
+            let nextIndex = (index + 1) % seating.count
+
+            let previousName = seating[previousIndex]
+            let nextName = seating[nextIndex]
+
+            let previousScore = scoring.first { $0.b == previousName }!.score
+            let nextScore = scoring.first { $0.b == nextName }!.score
+
+            score += previousScore + nextScore
+        }
+
+        return score
     }
 
     func solvePart1() -> Any {
-        return 0
+        let pairs = input.pairs
+        let names: [String] = Array(pairs.keys)
+
+        let allCombinations = names.permutations
+
+        var bestScore = Int.min
+
+        for seating in allCombinations {
+            bestScore = max(bestScore, calculateHappiness(seating: seating, pairs: pairs))
+        }
+
+        return bestScore
     }
 
     func solvePart2() -> Any {
-        return 0
+        var pairs = input.pairs
+        var names: [String] = Array(pairs.keys)
+
+        let newName = "Robert"
+
+        pairs[newName] = names.reduce(into: []) { result, name in
+            result.append(.init(a: newName, b: name, score: 0))
+        }
+
+        for name in names {
+            pairs[name]!.append(.init(a: name, b: newName, score: 0))
+        }
+
+        names.append(newName)
+
+        let allCombinations = names.permutations
+
+        var bestScore = Int.min
+
+        for seating in allCombinations {
+            bestScore = max(bestScore, calculateHappiness(seating: seating, pairs: pairs))
+        }
+
+        return bestScore
     }
 
     func parseInput(rawString: String) {
-        input = .init()
+        var pairs: [String: [Pair]] = [:]
+
+        rawString.allLines().forEach { line in
+            guard let parameters = line.getCapturedValues(pattern: #"([a-zA-Z]*) would ([a-zA-Z]*) ([0-9]*) happiness units by sitting next to ([a-zA-Z]*)"#) else {
+                fatalError()
+            }
+
+            let a = parameters[0]
+            let b = parameters[3]
+
+            let score = Int(parameters[2])!
+
+            let sign: Int = parameters[1] == "gain" ? 1 : -1
+
+            pairs[a, default: []] += [.init(a: a, b: b, score: sign * score)]
+        }
+
+        input = .init(pairs: pairs)
     }
 }
--- Sources/Solutions/2015/Days/Day14.swift
+++ Sources/Solutions/2015/Days/Day14.swift
@@ -7,17 +7,77 @@ final class Day14Solver: DaySolver {
     private var input: Input!
 
     private struct Input {
+        let reindeer: [Reindeer]
+    }
+
+    private struct Reindeer {
+        let name: String
+        let speed: Int
+        let duration: Int
+        let rest: Int
+    }
+
+    private func calculateDistanceTraveled(for reindeer: Reindeer, time: Int) -> Int {
+        let totalCycleDuration = reindeer.duration + reindeer.rest
+
+        let nrOfCompleteCycles = time / totalCycleDuration
+        let cycleRemainder = time % totalCycleDuration
+
+        return ((nrOfCompleteCycles * reindeer.duration) + min(cycleRemainder, reindeer.duration)) * reindeer.speed
     }
 
     func solvePart1() -> Any {
-        return 0
+        let time = 2503
+
+        var best: (name: String, distance: Int) = (name: "", distance: Int.min)
+
+        for reindeer in input.reindeer {
+            let distance = calculateDistanceTraveled(for: reindeer, time: time)
+
+            if distance > best.distance {
+                best = (name: reindeer.name, distance: distance)
+            }
+        }
+
+        return best
     }
 
     func solvePart2() -> Any {
-        return 0
+        let time = 2503
+
+        var scores: [String: Int] = [:]
+
+        for seconds in 1 ... time {
+            var best: (name: String, distance: Int) = (name: "", distance: Int.min)
+
+            for reindeer in input.reindeer {
+                let distance = calculateDistanceTraveled(for: reindeer, time: seconds)
+
+                if distance > best.distance {
+                    best = (name: reindeer.name, distance: distance)
+                }
+            }
+
+            scores[best.name, default: 0] += 1
+        }
+
+        return scores.sorted(by: { $0.value > $1.value } ).first!.value
     }
 
     func parseInput(rawString: String) {
-        input = .init()
+        let reindeer: [Reindeer] = rawString.allLines().map { line in
+            guard let parameters = line.getCapturedValues(pattern: #"([a-zA-Z]*) can fly ([0-9]*) km/s for ([0-9]*) seconds, but then must rest for ([0-9]*) seconds*"#) else {
+                fatalError()
+            }
+
+            let name = parameters[0]
+            let speed = Int(parameters[1])!
+            let duration = Int(parameters[2])!
+            let rest = Int(parameters[3])!
+
+            return .init(name: name, speed: speed, duration: duration, rest: rest)
+        }
+
+        input = .init(reindeer: reindeer)
     }
 }
