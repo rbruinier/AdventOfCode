838d684fe44876364e308f2bd40a9206b2b3d4ba
rbruinier <rjbruinier@gmail.com>
2022-02-13 17:19:52 +0100

    Day 16 & 17 2015

--- Sources/Solutions/2015/Days/Day16.swift
+++ Sources/Solutions/2015/Days/Day16.swift
@@ -7,17 +7,118 @@ final class Day16Solver: DaySolver {
     private var input: Input!
 
     private struct Input {
+        let aunts: [Aunt]
+    }
+    
+    private struct Aunt {
+        let name: String
+        let items: [String: Int]
     }
 
+    private let auntItems: [String: Int] = [
+        "children": 3,
+        "cats": 7,
+        "samoyeds": 2,
+        "pomeranians": 3,
+        "akitas": 0,
+        "vizslas": 0,
+        "goldfish": 5,
+        "trees": 3,
+        "cars": 2,
+        "perfumes": 1
+    ]
+
     func solvePart1() -> Any {
-        return 0
+        var bestAunt: Aunt!
+        var bestScore: Int = Int.min
+        
+        for aunt in input.aunts {
+            var fullMatchCount = 0
+            var semiMatchCount = 0
+            
+            for (item, number) in auntItems {
+                if let auntItemNumber = aunt.items[item] {
+                    if number == auntItemNumber {
+                        fullMatchCount += 1
+                    }
+                } else {
+                    if number == 0 {
+                        semiMatchCount += 1
+                    }
+                }
+            }
+            
+            let score = fullMatchCount + semiMatchCount
+            
+            if score > bestScore {
+                bestAunt = aunt
+                bestScore = score
+            }
+        }
+        
+        return Int(bestAunt.name.replacingOccurrences(of: "Sue ", with: ""))!
     }
 
     func solvePart2() -> Any {
-        return 0
+        var bestAunt: Aunt!
+        var bestScore: Int = Int.min
+        
+        for aunt in input.aunts {
+            var fullMatchCount = 0
+            var semiMatchCount = 0
+            
+            for (item, number) in auntItems {
+                if let auntItemNumber = aunt.items[item] {
+                    if ["cats", "trees"].contains(item) {
+                        if auntItemNumber > number {
+                            fullMatchCount += 1
+                        }
+                    } else if ["pomeranians", "goldfish"].contains(item) {
+                        if auntItemNumber < number {
+                            fullMatchCount += 1
+                        }
+                    } else {
+                        if number == auntItemNumber {
+                            fullMatchCount += 1
+                        }
+                    }
+                } else {
+                    if number == 0 {
+                        semiMatchCount += 1
+                    }
+                }
+            }
+            
+            let score = fullMatchCount + semiMatchCount
+            
+            if score > bestScore {
+                bestAunt = aunt
+                bestScore = score
+            }
+        }
+        
+        return Int(bestAunt.name.replacingOccurrences(of: "Sue ", with: ""))!        
     }
 
     func parseInput(rawString: String) {
-        input = .init()
+        let aunts: [Aunt] = rawString.allLines().map { line in
+            let components = line.components(separatedBy: ": ")
+
+            let items: [String: Int] = Dictionary(
+                uniqueKeysWithValues:
+                    components[1 ..< components.count]
+                        .joined(separator: ": ")
+                        .components(separatedBy: ", ")
+                        .map { itemString in
+                            let parts = itemString.components(separatedBy: ": ")
+                
+                            return (key: parts[0], value: Int(parts[1])!)
+                        }
+            )
+            
+            return .init(name: components[0], items: items)
+        }
+        
+        input = .init(aunts: aunts)
     }
 }
--- Sources/Solutions/2015/Days/Day17.swift
+++ Sources/Solutions/2015/Days/Day17.swift
@@ -7,17 +7,41 @@ final class Day17Solver: DaySolver {
     private var input: Input!
 
     private struct Input {
+        let containers: [Int]
+    }
+
+    func fill(containers: [Int], amount: Int, numberOfContainers: Int, solutionsPerContainers: inout [Int: Int]) {
+        for (index, container) in containers.enumerated() {
+            if amount == container {
+                solutionsPerContainers[numberOfContainers + 1, default: 0] += 1
+            } else if amount > container {
+                fill(
+                    containers: Array(containers[index + 1 ..< containers.count]),
+                    amount: amount - container,
+                    numberOfContainers: numberOfContainers + 1,
+                    solutionsPerContainers: &solutionsPerContainers
+                )
+            }
+        }
     }
 
     func solvePart1() -> Any {
-        return 0
+        var solutionsPerContainers: [Int: Int] = [:]
+
+        fill(containers: input.containers, amount: 150, numberOfContainers: 0, solutionsPerContainers: &solutionsPerContainers)
+
+        return solutionsPerContainers.values.reduce(0, +)
     }
 
     func solvePart2() -> Any {
-        return 0
+        var solutionsPerContainers: [Int: Int] = [:]
+
+        fill(containers: input.containers, amount: 150, numberOfContainers: 0, solutionsPerContainers: &solutionsPerContainers)
+
+        return solutionsPerContainers.sorted(by: { $0.key < $1.key }).first!.value
     }
 
     func parseInput(rawString: String) {
-        input = .init()
+        input = .init(containers: rawString.allLines().map { Int($0)! })
     }
 }
