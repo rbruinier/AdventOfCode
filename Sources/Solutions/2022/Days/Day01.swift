7740744bb632bef3f3dac929081cb5a0a4dd0a3a
Robert Bruinier <rjbruinier@gmail.com>
2022-12-01 09:13:22 +0100

    Day 1 2022

--- Sources/Solutions/2022/Days/Day01.swift
+++ Sources/Solutions/2022/Days/Day01.swift
@@ -2,22 +2,53 @@ import Foundation
 import Tools
 
 final class Day01Solver: DaySolver {
-    let dayNumber: Int = 1
+	let dayNumber: Int = 1
 
-    private var input: Input!
+	private var input: Input!
 
-    private struct Input {
-    }
+	private struct Input {
+		let elves: [Elf]
+	}
 
-    func solvePart1() -> Any {
-        return 0
-    }
+	private struct Elf {
+		var items: [Int]
 
-    func solvePart2() -> Any {
-        return 0
-    }
+		var total: Int {
+			items.reduce(0, +)
+		}
+	}
 
-    func parseInput(rawString: String) {
-        input = .init()
-    }
+	func solvePart1() -> Any {
+		input.elves
+			.sorted(by: { $0.total < $1.total })
+			.last!
+			.total
+	}
+
+	func solvePart2() -> Any {
+		input.elves
+			.sorted(by: { $0.total < $1.total })[input.elves.count - 3 ..< input.elves.count]
+			.reduce(0) { $0 + $1.total }
+	}
+
+	func parseInput(rawString: String) {
+		var elves: [Elf] = []
+
+		var items: [Int] = []
+		for line in rawString.allLines(includeEmpty: true) {
+			if line.isEmpty {
+				elves.append(.init(items: items))
+
+				items.removeAll()
+			} else {
+				items.append(Int(line)!)
+			}
+		}
+
+		if items.isNotEmpty {
+			elves.append(.init(items: items))
+		}
+
+		input = .init(elves: elves)
+	}
 }
