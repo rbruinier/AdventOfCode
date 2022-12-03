0aab184d3a8fb6aa4b2ac897c97c0dafd37ce1af
Robert Bruinier <rjbruinier@gmail.com>
2022-12-03 15:17:35 +0100

    Day 9 of 2017

--- Sources/Solutions/2017/Days/Day09.swift
+++ Sources/Solutions/2017/Days/Day09.swift
@@ -7,17 +7,62 @@ final class Day09Solver: DaySolver {
     private var input: Input!
 
     private struct Input {
+        let text: String
+    }
+
+    private func solve(text: String) -> (sum: Int, nrOfCancelled: Int) {
+        var inGarbage = false
+        var nextIsNegative = false
+
+        var groupDepth = 0
+        var sum = 0
+        var nrOfCancelled = 0
+
+        for character in input.text {
+            if inGarbage {
+                if character == ">" {
+                    if nextIsNegative {
+                        nextIsNegative = false
+
+                        continue
+                    } else {
+                        inGarbage = false
+                    }
+                } else if character == "!" {
+                    nextIsNegative.toggle()
+                } else {
+                    if !nextIsNegative {
+                        nrOfCancelled += 1
+                    }
+
+                    nextIsNegative = false
+                }
+            } else {
+                if character == "{" {
+                    groupDepth += 1
+                } else if character == "}" {
+                    sum += groupDepth
+
+                    groupDepth -= 1
+                } else if character == "<" {
+                    inGarbage = true
+                    nextIsNegative = false
+                }
+            }
+        }
+
+        return (sum: sum, nrOfCancelled: nrOfCancelled)
     }
 
     func solvePart1() -> Any {
-        return 0
+        solve(text: input.text).sum
     }
 
     func solvePart2() -> Any {
-        return 0
+        solve(text: input.text).nrOfCancelled
     }
 
     func parseInput(rawString: String) {
-        input = .init()
+        input = .init(text: rawString.allLines().first!)
     }
 }
