5e02f5c2bef2d61930b13b7315434968492763bb
Robert Bruinier <rjbruinier@gmail.com>
2023-01-12 19:28:47 +0100

    Day 7 & 8 2018

--- Sources/Solutions/2018/Days/Day08.swift
+++ Sources/Solutions/2018/Days/Day08.swift
@@ -10,17 +10,68 @@ final class Day08Solver: DaySolver {
     private var input: Input!
 
     private struct Input {
+        let node: Node
+    }
+
+    private final class Node {
+        var childNodes: [Node] = []
+        var metadataEntries: [Int] = []
+
+        init() {
+        }
+    }
+
+    private func sumOfMetadata(node: Node) -> Int {
+        node.metadataEntries.reduce(0, +) + node.childNodes.reduce(0) { $0 + sumOfMetadata(node: $1) }
+    }
+
+    private func valueOf(node: Node) -> Int {
+        if node.childNodes.isEmpty {
+            return node.metadataEntries.reduce(0, +)
+        } else {
+            return node.metadataEntries.reduce(into: 0) { result, nodeId in
+                if let childNode = node.childNodes[safe: nodeId - 1] {
+                    result += valueOf(node: childNode)
+                }
+            }
+        }
     }
 
     func solvePart1() -> Int {
-        return 0
+        sumOfMetadata(node: input.node)
     }
 
     func solvePart2() -> Int {
-        return 0
+        valueOf(node: input.node)
     }
 
     func parseInput(rawString: String) {
-        input = .init()
+        func parseNode(at position: inout Int, data: [Int]) -> Node {
+            let node = Node()
+
+            let numberOfChildNodes = data[position]
+
+            position += 1
+
+            let numberOfMetadataEntries = data[position]
+
+            position += 1
+
+            for _ in 0 ..< numberOfChildNodes {
+                node.childNodes.append(parseNode(at: &position, data: data))
+            }
+
+            for _ in 0 ..< numberOfMetadataEntries {
+                node.metadataEntries.append(data[position])
+
+                position += 1
+            }
+
+            return node
+        }
+
+        var position = 0
+
+        input = .init(node: parseNode(at: &position, data: rawString.allLines().first!.components(separatedBy: " ").map { Int($0)! }))
     }
 }
