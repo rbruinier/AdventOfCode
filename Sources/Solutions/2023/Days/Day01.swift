f16fbbf58b9c556292e4f288b78026d0f0442a7f
Robert Bruinier <rjbruinier@gmail.com>
2023-12-01 19:36:03 +0100

    Add day 1 of 2023

--- Sources/Solutions/2023/Days/Day01.swift
+++ Sources/Solutions/2023/Days/Day01.swift
@@ -2,25 +2,63 @@ import Foundation
 import Tools
 
 final class Day01Solver: DaySolver {
-    let dayNumber: Int = 1
+	let dayNumber: Int = 1
 
-    let expectedPart1Result = 0
-    let expectedPart2Result = 0
+	let expectedPart1Result = 54159
+	let expectedPart2Result = 53866
 
-    private var input: Input!
+	private var input: Input!
 
-    private struct Input {
-    }
+	private struct Input {
+		let lines: [String]
+	}
 
-    func solvePart1() -> Int {
-        return 0
-    }
+	func solvePart1() -> Int {
+		input.lines.reduce(0) { result, value in
+			let digits = value.compactMap { Int(String($0)) }
 
-    func solvePart2() -> Int {
-        return 0
-    }
+			return result + (digits.first! * 10) + digits.last!
+		}
+	}
 
-    func parseInput(rawString: String) {
-        input = .init()
-    }
+	func solvePart2() -> Int {
+		let mappings: [String: Int] = [
+			"one": 1,
+			"two": 2,
+			"three": 3,
+			"four": 4,
+			"five": 5,
+			"six": 6,
+			"seven": 7,
+			"eight": 8,
+			"nine": 9,
+		]
+
+		var sum = 0
+		for line in input.lines {
+			var digits: [Int] = []
+
+			lineLoop: for position in 0 ..< line.count {
+				if let digit = Int(String(line[position])) {
+					digits.append(digit)
+				} else {
+					for mapping in mappings {
+						if line[position ..< line.count].hasPrefix(mapping.key) {
+							digits.append(mapping.value)
+
+							break
+						}
+					}
+				}
+			}
+
+			sum += (digits.first! * 10) + digits.last!
+		}
+
+		return sum
+	}
+
+	func parseInput(rawString: String) {
+		input = .init(lines: rawString.allLines())
+	}
 }
