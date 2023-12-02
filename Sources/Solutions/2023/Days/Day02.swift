2a2dddd5426b9eb04aa5cf65740596e32fe906ba
Robert Bruinier <rjbruinier@gmail.com>
2023-12-02 08:50:14 +0100

    Solution for day 02 of 2023

--- Sources/Solutions/2023/Days/Day02.swift
+++ Sources/Solutions/2023/Days/Day02.swift
@@ -10,17 +10,85 @@ final class Day02Solver: DaySolver {
     private var input: Input!
 
     private struct Input {
+        let games: [Game]
+    }
+
+    private struct Game {
+        struct Set {
+            let red: Int
+            let green: Int
+            let blue: Int
+        }
+
+        let id: Int
+        let sets: [Set]
+
+        func isValid(withMaxRed maxRed: Int, maxGreen: Int, maxBlue: Int) -> Bool {
+            !sets.contains { set in
+                set.red > maxRed || set.green > maxGreen || set.blue > maxBlue
+            }
+        }
     }
 
     func solvePart1() -> Int {
-        return 0
+        let maxRed = 12
+        let maxGreen = 13
+        let maxBlue = 14
+
+        return input.games
+            .filter { $0.isValid(withMaxRed: maxRed, maxGreen: maxGreen, maxBlue: maxBlue) }
+            .reduce(0) { result, game in result + game.id }
     }
 
     func solvePart2() -> Int {
-        return 0
+        return input.games.reduce(0) { result, game in
+            let maxRed = game.sets.map(\.red).max()!
+            let maxGreen = game.sets.map(\.green).max()!
+            let maxBlue = game.sets.map(\.blue).max()!
+
+            return result + maxRed * maxGreen * maxBlue
+        }
     }
 
     func parseInput(rawString: String) {
-        input = .init()
+        var gameID = 1
+
+        let games: [Game] = rawString.allLines().map { line in
+            defer {
+                gameID += 1
+            }
+
+            let rawSets = line
+                .components(separatedBy: ": ")[1]
+                .trimmingCharacters(in: .whitespacesAndNewlines)
+                .components(separatedBy: ";")
+
+            let sets: [Game.Set] = rawSets.map { rawSet in
+                let rawCubes = rawSet.components(separatedBy: ", ")
+
+                var red = 0
+                var green = 0
+                var blue = 0
+
+                for rawCube in rawCubes {
+                    let components = rawCube
+                        .trimmingCharacters(in: .whitespacesAndNewlines)
+                        .components(separatedBy: " ")
+
+                    switch components[1] {
+                    case "red": red += Int(components[0])!
+                    case "green": green += Int(components[0])!
+                    case "blue": blue += Int(components[0])!
+                    default: preconditionFailure()
+                    }
+                }
+
+                return .init(red: red, green: green, blue: blue)
+            }
+
+            return .init(id: gameID, sets: sets)
+        }
+
+        input = .init(games: games)
     }
 }
