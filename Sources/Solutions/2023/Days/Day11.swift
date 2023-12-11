b0edaff7ba2145eaf6ccd9c527f8b98843a35473
Robert Bruinier <rjbruinier@gmail.com>
2023-12-11 06:34:27 +0100

    Day 11 of 2023

--- Sources/Solutions/2023/Days/Day11.swift
+++ Sources/Solutions/2023/Days/Day11.swift
@@ -4,22 +4,100 @@ import Tools
 final class Day11Solver: DaySolver {
 	let dayNumber: Int = 11
 
-	let expectedPart1Result = 0
-	let expectedPart2Result = 0
+	let expectedPart1Result = 9565386
+	let expectedPart2Result = 857986849428
 
 	private var input: Input!
 
-	private struct Input {}
+	private struct Input {
+		let galaxies: Set<Point2D>
+	}
+
+	func expandColumns(in galaxies: Set<Point2D>, factor: Int) -> Set<Point2D> {
+		var newGalaxies: Set<Point2D> = []
+
+		let maxX = galaxies.map(\.x).max()!
+
+		var newX = 0
+		for x in 0 ... maxX {
+			let existingGalaxiesOnColumn = galaxies.filter { $0.x == x }
+
+			if existingGalaxiesOnColumn.isEmpty {
+				newX += factor
+			} else {
+				newGalaxies = newGalaxies.union(existingGalaxiesOnColumn.map { .init(x: newX, y: $0.y) })
+
+				newX += 1
+			}
+		}
+
+		return newGalaxies
+	}
+
+	func expandRows(in galaxies: Set<Point2D>, factor: Int) -> Set<Point2D> {
+		var newGalaxies: Set<Point2D> = []
+
+		let maxY = galaxies.map(\.y).max()!
+
+		var newY = 0
+		for y in 0 ... maxY {
+			let existingGalaxiesOnColumn = galaxies.filter { $0.y == y }
+
+			if existingGalaxiesOnColumn.isEmpty {
+				newY += factor
+			} else {
+				newGalaxies = newGalaxies.union(existingGalaxiesOnColumn.map { .init(x: $0.x, y: newY) })
+
+				newY += 1
+			}
+		}
+
+		return newGalaxies
+	}
+
+	private func calculateTotalDistance(with originalGalaxies: Set<Point2D>, factor: Int) -> Int {
+		var expandedGalaxies = originalGalaxies
+
+		expandedGalaxies = expandColumns(in: expandedGalaxies, factor: factor)
+		expandedGalaxies = expandRows(in: expandedGalaxies, factor: factor)
+
+		let galaxies = Array(expandedGalaxies)
+
+		var sum = 0
+		for i in 0 ..< galaxies.count {
+			for j in i + 1 ..< galaxies.count {
+				let distance = galaxies[i].manhattanDistance(from: galaxies[j])
+
+				sum += distance
+			}
+		}
+
+		return sum
+	}
 
 	func solvePart1() -> Int {
-		0
+		calculateTotalDistance(with: input.galaxies, factor: 2)
 	}
 
 	func solvePart2() -> Int {
-		0
+		calculateTotalDistance(with: input.galaxies, factor: 1_000_000)
 	}
 
 	func parseInput(rawString: String) {
-		input = .init()
+		var galaxies: Set<Point2D> = []
+
+		rawString.allLines().enumerated().forEach { y, line in
+			line.enumerated().forEach { x, character in
+				let point = Point2D(x: x, y: y)
+
+				switch character {
+				case ".": break
+				case "#": galaxies.insert(point)
+				default: preconditionFailure()
+				}
+			}
+		}
+
+		input = .init(galaxies: galaxies)
 	}
 }
