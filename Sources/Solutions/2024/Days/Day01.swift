f229a1b338e072d59f8a4f82c81c18355699fa75
Robert Bruinier <rjbruinier@gmail.com>
2024-12-01 17:51:30 +0100

    Day 1 of 2024

--- Sources/Solutions/2024/Days/Day01.swift
+++ Sources/Solutions/2024/Days/Day01.swift
@@ -2,25 +2,65 @@ import Foundation
 import Tools
 
 final class Day01Solver: DaySolver {
-    let dayNumber: Int = 1
+	let dayNumber: Int = 1
 
-	let expectedPart1Result = 0
-	let expectedPart2Result = 0
+	let expectedPart1Result = 1388114
+	let expectedPart2Result = 23529853
 
 	private var input: Input!
-	
-    private struct Input {
-    }
 
-    func solvePart1() -> Int {
-        return 0
-    }
+	private struct Input {
+		let listA: [Int]
+		let listB: [Int]
+	}
 
-    func solvePart2() -> Int {
-        return 0
-    }
+	func solvePart1() -> Int {
+		let aValues: [Int] = input.listA.sorted()
+		let bValues: [Int] = input.listB.sorted()
 
-    func parseInput(rawString: String) {
-        input = .init()
-    }
+		var totalDistance = 0
+		for (a, b) in zip(aValues, bValues) {
+			totalDistance += abs(a - b)
+		}
+
+		return totalDistance
+	}
+
+	func solvePart2() -> Int {
+		let aValues: [Int] = input.listA
+		let bValues: [Int] = input.listB
+
+		var totalScore = 0
+		for a in aValues {
+			let occurencesInB = bValues.count { $0 == a }
+
+			totalScore += a * occurencesInB
+		}
+
+		return totalScore
+	}
+
+	func parseInput(rawString: String) {
+		var listA: [Int] = []
+		var listB: [Int] = []
+
+		for line in rawString.allLines() {
+			let components = line.components(separatedBy: .whitespaces)
+
+			var listIndex = 0
+			for component in components where !component.isEmpty {
+				assert(listIndex <= 1)
+
+				if listIndex == 0 {
+					listA.append(Int(component)!)
+				} else {
+					listB.append(Int(component)!)
+				}
+
+				listIndex += 1
+			}
+		}
+
+		input = .init(listA: listA, listB: listB)
+	}
 }
