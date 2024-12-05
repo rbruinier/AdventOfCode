fca815b2f36700a9430cac65f47c6be73808b466
Robert Bruinier <rjbruinier@gmail.com>
2024-12-05 09:09:36 +0100

    Day 5 of 2024

--- Sources/Solutions/2024/Days/Day05.swift
+++ Sources/Solutions/2024/Days/Day05.swift
@@ -2,25 +2,113 @@ import Foundation
 import Tools
 
 final class Day05Solver: DaySolver {
-    let dayNumber: Int = 5
+	let dayNumber: Int = 5
 
-	let expectedPart1Result = 0
-	let expectedPart2Result = 0
+	let expectedPart1Result = 5087
+	let expectedPart2Result = 4971
 
-    private var input: Input!
+	private var input: Input!
 
-    private struct Input {
-    }
+	private struct Rule {
+		let a: Int
+		let b: Int
+	}
 
-    func solvePart1() -> Int {
-        return 0
-    }
+	private struct Input {
+		let rules: [Rule]
+		let updates: [[Int]]
+	}
 
-    func solvePart2() -> Int {
-        return 0
-    }
+	private func isValidUpdate(_ update: [Int], rules: [Rule]) -> Bool {
+		for (index, value) in update.enumerated() {
+			for rule in rules.filter({ $0.a == value }) {
+				if let bIndex = update.firstIndex(of: rule.b), index > bIndex {
+					return false
+				}
+			}
 
-    func parseInput(rawString: String) {
-        input = .init()
-    }
+			for rule in rules.filter({ $0.b == value }) {
+				if let aIndex = update.firstIndex(of: rule.a), index < aIndex {
+					return false
+				}
+			}
+		}
+
+		return true
+	}
+
+	private func fixUpdate(_ update: [Int], rules: [Rule]) -> [Int] {
+		var currentUpdate = update
+
+		fixLoop: while true {
+			var newUpdate: [Int] = currentUpdate
+
+			for (index, value) in currentUpdate.enumerated() {
+				for rule in rules.filter({ $0.a == value }) {
+					if let bIndex = currentUpdate.firstIndex(of: rule.b), index > bIndex {
+						newUpdate.remove(at: index)
+						newUpdate.insert(value, at: bIndex)
+
+						currentUpdate = newUpdate
+
+						continue fixLoop
+					}
+				}
+
+				for rule in rules.filter({ $0.b == value }) {
+					if let aIndex = currentUpdate.firstIndex(of: rule.a), index < aIndex {
+						newUpdate.insert(value, at: aIndex + 1)
+						newUpdate.remove(at: index)
+
+						currentUpdate = newUpdate
+
+						continue fixLoop
+					}
+				}
+			}
+
+			break
+		}
+
+		return currentUpdate
+	}
+
+	func solvePart1() -> Int {
+		input.updates.map {
+			isValidUpdate($0, rules: input.rules) ? $0[$0.count >> 1] : 0
+		}.reduce(0, +)
+	}
+
+	func solvePart2() -> Int {
+		input.updates
+			.filter { !isValidUpdate($0, rules: input.rules) }
+			.map {
+				fixUpdate($0, rules: input.rules)[$0.count >> 1]
+			}
+			.reduce(0, +)
+	}
+
+	func parseInput(rawString: String) {
+		var rules: [Rule] = []
+		var updates: [[Int]] = []
+
+		var parsingRules = true
+		for line in rawString.allLines(includeEmpty: true) {
+			if line.isEmpty {
+				parsingRules = false
+
+				continue
+			}
+
+			if parsingRules {
+				let parts = line.split(separator: "|")
+
+				rules.append(.init(a: Int(parts[0])!, b: Int(parts[1])!))
+			} else {
+				updates.append(line.parseCommaSeparatedInts())
+			}
+		}
+
+		input = .init(rules: rules, updates: updates)
+	}
 }
