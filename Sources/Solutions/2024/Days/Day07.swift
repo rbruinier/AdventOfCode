b485684bd890c7e3334e58d12cd5f14fb4171cb9
Robert Bruinier <rjbruinier@gmail.com>
2024-12-07 09:10:25 +0100

    Use concurrency in part 2 day 7 2024 to speed up calculations

--- Sources/Solutions/2024/Days/Day07.swift
+++ Sources/Solutions/2024/Days/Day07.swift
@@ -18,7 +18,7 @@ final class Day07Solver: DaySolver {
 		let equations: [Equation]
 	}
 
-	private func solveEquation(expectedResult: Int, operands: [Int], currentValue: Int, allowConcatenation: Bool) -> Bool {
+	private static func solveEquation(expectedResult: Int, operands: [Int], currentValue: Int, allowConcatenation: Bool) -> Bool {
 		guard let nextOperand = operands.first, currentValue <= expectedResult else {
 			return expectedResult == currentValue
 		}
@@ -43,14 +43,20 @@ final class Day07Solver: DaySolver {
 
 	func solvePart1() -> Int {
 		input.equations.filter {
-			solveEquation(expectedResult: $0.result, operands: Array($0.operands[1...]), currentValue: $0.operands[0], allowConcatenation: false)
+			Self.solveEquation(expectedResult: $0.result, operands: Array($0.operands[1...]), currentValue: $0.operands[0], allowConcatenation: false)
 		}.map(\.result).reduce(0, +)
 	}
 
-	func solvePart2() -> Int {
-		input.equations.filter {
-			solveEquation(expectedResult: $0.result, operands: Array($0.operands[1...]), currentValue: $0.operands[0], allowConcatenation: true)
-		}.map(\.result).reduce(0, +)
+	func solvePart2() async -> Int {
+		await withTaskGroup(of: Int.self, returning: Int.self) { taskGroup in
+			for equation in input.equations {
+				taskGroup.addTask {
+					Self.solveEquation(expectedResult: equation.result, operands: Array(equation.operands[1...]), currentValue: equation.operands[0], allowConcatenation: true) ? equation.result : 0
+				}
+			}
+
+			return await taskGroup.reduce(into: 0) { $0 += $1 }
+		}
 	}
 
 	func parseInput(rawString: String) {
